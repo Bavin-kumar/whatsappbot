45fa99ebfdd3a97ee4b0bee0bb240f51389c7185
Bavin-kumar <90148716+Bavin-kumar@users.noreply.github.com>
2021-09-10 09:25:50 +0530

    Update whatsappbot.py

--- whatsappbot.py
+++ whatsappbot.py
@@ -1,4 +1,4 @@
 import pywhatkit
 
-pywhatkit.sendwhatmsg('+919786354311', 'hi', 9, 50)
+pywhatkit.sendwhatmsg('mobile_number', 'hi', 9, 50) # mobile_number = '+9198654321' number with country code
 print('message send')
